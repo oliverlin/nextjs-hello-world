50ef39ee2ca9926b27924bc2fd41508a412d7795
Oliver Lin <linchong223@gmail.com>
2021-11-16 14:38:41 +0800

    Add home btn

--- pages/about.js
+++ pages/about.js
@@ -1,2 +1,10 @@
-const About = () => <div>About us</div>;
+import Link from 'next/link'
+
+const About = () => <div>
+  About us
+  <Link href="/">
+    <a>Home</a>
+  </Link>
+</div>;
+
 export default About;
